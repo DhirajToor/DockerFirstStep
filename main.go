fe958d8584201f029ee9275929e613b6ec6be9e4
Zubiyan <113921210+Zubiyan@users.noreply.github.com>
2023-09-28 19:52:20 -0400

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import "log"
 func Dhiraj() {
 	log.Println("This funcion created by Dhiraj Toor")
 }
-
+func Mohammad() {
+	log.Println("This funcion created by Mohammad Zubiyan")
+}
 func main() {
 	Dhiraj()
 }
